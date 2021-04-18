840689389866e73851640e521a1d0909b3e0852f
athapar <armaant.08@gmail.com>
2021-04-18 00:51:21 -0400

    Removing deprecated code

--- Code/scan_inputs.py
+++
@@ -1,9 +0,0 @@
-import json
-import ipywidgets as wg
-import tkinter as tk
-from IPython.display import display
-
-fields = 'Number of Molecules', 'Molecular Size', 'Trials', 'L'
-
-name = wg.Text(value='Name')
-display(name)
